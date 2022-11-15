0f8c56de676edb8c862bd6ddcc72f9960384e049
Vishnu1119v <105714872+Vishnu1119v@users.noreply.github.com>
2022-11-15 15:06:46 +0530

    question 3

---
+++ count_odd_even.py
@@ -0,0 +1,10 @@
+lst=[0,1,2,3,4,5,6,7,8,9]
+even=0
+odd=0
+for i in lst:
+    if i%2==0:
+        even+=1
+    else:
+        odd+=1
+print("count of odd numbers=",odd)
+print("count of eve numbers=",even)
